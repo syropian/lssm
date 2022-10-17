8ea86af88e9b0c26864d8fbc55e53b5a5feed66a
Collin Henderson <collinpatrickhenderson@gmail.com>
2022-10-17 19:16:12 -0400

    Basic logic, tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,135 @@
-export default function (first: number, second: number): number {
-  return first + second
+export interface Config {
+  altKey?: boolean
+  ctrlKey?: boolean
+  metaKey?: boolean
+  shiftKey?: boolean
+}
+
+export default class<T> {
+  /**
+   * Private members
+   */
+  private items = [] as T[]
+
+  private selectedItems = [] as T[]
+
+  private lastSelectedItem: T | null = null
+  private lastNonShiftSelectedItem: T | null = null
+
+  private defaultConfig: Config = {
+    ctrlKey: false,
+    metaKey: false,
+    shiftKey: false,
+  }
+
+  /**
+   * Private functions
+   */
+
+  private setSelectedItemRange(item: T) {
+    if (!this.selectedItems.length) {
+      const index = this.items.indexOf(item)
+
+      if (index < 0) return
+
+      this.selectedItems = this.items.slice(0, index + 1)
+    } else {
+      const lastItem = this.lastSelectedItem as T
+      const lastIndex = this.items.indexOf(lastItem)
+      const lastNonShiftItem = this.lastNonShiftSelectedItem as T
+      const lastNonShiftIndex = this.items.indexOf(lastNonShiftItem)
+      const newIndex = this.items.indexOf(item)
+      const startIndex = Math.min(lastNonShiftIndex, newIndex)
+      const endIndex = Math.max(lastNonShiftIndex, newIndex) + 1
+
+      // We want to keep any items selected items that are not adjacent to the range
+      const keepItems = this.selectedItems.filter(item => {
+        const index = this.items.indexOf(item)
+        if (newIndex < lastNonShiftIndex) {
+          return index <= newIndex || index >= lastIndex + 2
+        } else {
+          return index < lastIndex - 2 || index > newIndex + 1
+        }
+      })
+
+      this.selectedItems = [
+        ...new Set([...keepItems, ...this.items.slice(startIndex, endIndex)]),
+      ].sort((a, b) => this.items.indexOf(a) - this.items.indexOf(b))
+    }
+  }
+
+  private setSingleSelectedItem(item: T) {
+    this.selectedItems = [item]
+  }
+
+  private toggleItem(item: T) {
+    if (this.itemIsSelected(item)) {
+      this.removeItemFromSelection(item)
+    } else {
+      this.appendItemsToSelection(item)
+    }
+  }
+
+  private removeItemFromSelection(item: T) {
+    this.selectedItems = this.selectedItems.filter(i => i !== item)
+  }
+
+  private appendItemsToSelection(item: T) {
+    if (!this.itemIsSelected(item)) {
+      this.selectedItems = [...this.selectedItems, item]
+    }
+  }
+
+  private itemIsSelected(item: T) {
+    return this.selectedItems.includes(item)
+  }
+
+  // private createRange(start: number, end: number) {
+  //   return Array.from({ length: end - start + 1 }, (_, i) => start + i)
+  // }
+
+  // private clamp(value: number, min: number, max: number) {
+  //   return Math.max(min, Math.min(value, max))
+  // }
+
+  /**
+   * Constructor
+   */
+  constructor(items: T[]) {
+    this.items = items
+  }
+
+  /**
+   * Public functions
+   */
+  public selectItem(item: T, config: Config = this.defaultConfig): T[] {
+    config = { ...this.defaultConfig, ...config }
+    const { ctrlKey, shiftKey } = config
+    const metaKey = config.metaKey || ctrlKey
+
+    if (metaKey) {
+      this.toggleItem(item)
+    } else if (shiftKey && !metaKey) {
+      this.setSelectedItemRange(item)
+    } else {
+      this.setSingleSelectedItem(item)
+    }
+
+    this.lastSelectedItem = item
+    // console.log('Last selection is', item)
+    if (!shiftKey) {
+      // console.log('Last non-shift selection is', item)
+      this.lastNonShiftSelectedItem = item
+    }
+
+    return this.selectedItems
+  }
+
+  public getSelectedItems() {
+    return this.selectedItems
+  }
+
+  public clearSelection() {
+    this.selectedItems = []
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,185 @@
 import { describe, test, expect } from 'vitest'
-import sum from '../src'
+import ListSelectionStateManager, { Config } from '../src'
 
-describe('my-module', () => {
-  test('returns the sum of the arguments', () => {
-    expect(sum(2, 3)).toBe(5)
+const range = (start: number, end: number) =>
+  Array.from({ length: end - start + 1 }, (_, i) => start + i)
+
+describe('list-selection-state', () => {
+  describe('Single-selection', () => {
+    test('Selecting a single item with no modifier key pressed ', () => {
+      const listFixture = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+      ]
+      const lssm = new ListSelectionStateManager(listFixture)
+
+      lssm.selectItem(5)
+      expect(lssm.getSelectedItems()).toEqual([5])
+
+      lssm.selectItem(9)
+      expect(lssm.getSelectedItems()).toEqual([9])
+    })
+
+    test('Selecting a single unselected item with the meta key pressed appends the item to the current selection', () => {
+      const listFixture = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+      ]
+      const config: Config = {
+        metaKey: true,
+      }
+      const lm = new ListSelectionStateManager(listFixture)
+
+      lm.selectItem(5, config)
+      expect(lm.getSelectedItems()).toEqual([5])
+
+      lm.selectItem(9, config)
+      expect(lm.getSelectedItems()).toEqual([5, 9])
+    })
+
+    test('Selecting a single selected item with the meta key pressed removes the item to the current selection', () => {
+      const listFixture = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+      ]
+      const config: Config = {
+        metaKey: true,
+      }
+      const lm = new ListSelectionStateManager(listFixture)
+
+      lm.selectItem(5, config)
+      lm.selectItem(6, config)
+      lm.selectItem(9, config)
+      lm.selectItem(5, config)
+
+      expect(lm.getSelectedItems()).toEqual([6, 9]) //nice
+    })
+  })
+
+  describe('Multi-selection', () => {
+    test('If no item is selected, all items from index 0 to the item that was clicked should be selected', () => {
+      const listFixture = range(0, 19)
+      const config: Config = {
+        shiftKey: true,
+      }
+      const lm = new ListSelectionStateManager(listFixture)
+
+      lm.selectItem(5, config)
+      expect(lm.getSelectedItems()).toEqual(range(0, 5))
+    })
+
+    test('If an item is already selected and a non-selected item is clicked, it should select all items between them, inclusive to both the initial selected item, and the item clicked', () => {
+      const listFixture = range(0, 19)
+      const config: Config = {
+        shiftKey: true,
+      }
+      const lm = new ListSelectionStateManager(listFixture)
+
+      lm.selectItem(5)
+      lm.selectItem(10, config)
+      expect(lm.getSelectedItems()).toEqual(range(5, 10))
+
+      lm.selectItem(13, config)
+      expect(lm.getSelectedItems()).toEqual(range(5, 13))
+
+      lm.clearSelection()
+
+      lm.selectItem(15)
+      lm.selectItem(10, config)
+      expect(lm.getSelectedItems()).toEqual(range(10, 15))
+
+      lm.selectItem(7, config)
+      expect(lm.getSelectedItems()).toEqual(range(7, 15))
+    })
+
+    test('Monkey test #1', () => {
+      const listFixture = range(0, 19)
+      const shiftConfig: Config = {
+        shiftKey: true,
+      }
+      const metaConfig: Config = {
+        metaKey: true,
+      }
+
+      const lm = new ListSelectionStateManager(listFixture)
+      lm.selectItem(2)
+      lm.selectItem(6, metaConfig)
+      expect(lm.getSelectedItems()).toEqual([2, 6])
+
+      lm.selectItem(12, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual([2, ...range(6, 12)])
+
+      lm.clearSelection()
+
+      lm.selectItem(2)
+      lm.selectItem(12, metaConfig)
+      lm.selectItem(7, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual([2, ...range(7, 12)])
+
+      lm.clearSelection()
+
+      lm.selectItem(10)
+      lm.selectItem(4, metaConfig)
+      lm.selectItem(14, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual(range(4, 14))
+
+      lm.clearSelection()
+
+      lm.selectItem(10)
+      lm.selectItem(14, metaConfig)
+      lm.selectItem(4, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual(range(4, 14))
+    })
+
+    test('Monkey test #2', () => {
+      const listFixture = range(0, 19)
+      const shiftConfig: Config = {
+        shiftKey: true,
+      }
+      const metaConfig: Config = {
+        metaKey: true,
+      }
+
+      const lm = new ListSelectionStateManager(listFixture)
+
+      lm.selectItem(10)
+      lm.selectItem(14, shiftConfig)
+      lm.selectItem(6, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual(range(6, 10))
+
+      lm.clearSelection()
+
+      lm.selectItem(2)
+      lm.selectItem(10, metaConfig)
+      lm.selectItem(14, shiftConfig)
+      lm.selectItem(6, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual([2, ...range(6, 10)])
+
+      lm.clearSelection()
+
+      lm.selectItem(18)
+      lm.selectItem(10, metaConfig)
+      lm.selectItem(14, shiftConfig)
+      lm.selectItem(6, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual([...range(6, 10), 18])
+
+      lm.clearSelection()
+
+      lm.selectItem(15)
+      lm.selectItem(10, metaConfig)
+      lm.selectItem(14, shiftConfig)
+      lm.selectItem(6, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual(range(6, 10))
+
+      lm.clearSelection()
+
+      lm.selectItem(10)
+      lm.selectItem(6, shiftConfig)
+      lm.selectItem(14, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual(range(10, 14))
+
+      lm.selectItem(18)
+      lm.selectItem(10, metaConfig)
+      lm.selectItem(6, shiftConfig)
+      lm.selectItem(14, shiftConfig)
+      expect(lm.getSelectedItems()).toEqual([...range(10, 14), 18])
+    })
   })
 })
