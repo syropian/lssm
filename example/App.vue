8e0054a0b56b6bf82f73a8483b8d55ef638ee859
Collin Henderson <collinpatrickhenderson@gmail.com>
2022-10-23 16:38:14 -0400

    More example improvements

--- example/App.vue
+++ example/App.vue
@@ -1,27 +1,50 @@
 <template>
   <div class="max-w-sm w-full mx-auto mt-20">
-    <h1 class="text-3xl font-semibold text-gray-700">lssm</h1>
-    <h2 class="text-gray-500 text-lg">
+    <div class="flex items-center justify-between">
+      <h1 class="text-3xl font-semibold text-gray-700">lssm</h1>
+      <a
+        href="https://github.com/syropian/lssm"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="inline-block text-gray-700 transition-colors hover:text-blue-500"
+        aria-label="GitHub"
+        ><svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke-width="2"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="w-6 h-6 stroke-current"
+        >
+          <path
+            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+          ></path></svg
+      ></a>
+    </div>
+    <h2 class="text-gray-500 text-lg mt-2">
       An opinionated list selection state manager
     </h2>
-    <p class="mt-4 text-gray-800 pt-4 border-t border-gray-100 leading-loose">
-      Click an item to select it. You may press
-      <kbd
-        class="rounded bg-gray-100 text-gray-700 p-1 text-xs border border-gray-300 shadow"
-        >cmd</kbd
-      >
-      or
-      <kbd
-        class="rounded bg-gray-100 text-gray-700 p-1 text-xs border border-gray-300 shadow"
-        >ctrl</kbd
-      >
-      to toggle individual items. You may also hold
-      <kbd
-        class="rounded bg-gray-100 text-gray-700 p-1 text-xs border border-gray-300 shadow"
-        >shift</kbd
-      >
-      to select a range of items.
-    </p>
+    <div class="mt-4 pt-4 border-t border-gray-100 leading-loose prose">
+      <ul>
+        <li>Click an item to select it</li>
+        <li>
+          <kbd class="text-xs">ctrl</kbd> or
+          <kbd class="text-xs">cmd</kbd> click an item to toggle it
+        </li>
+        <li>
+          <kbd class="text-xs">shift</kbd> or
+          <kbd class="text-xs">cmd</kbd> click an item to select the items as a
+          range
+        </li>
+        <li>Use the arrow keys to select the next or previous item</li>
+        <li>
+          Use <kbd class="text-xs">shift</kbd> plus the arrow keys to
+          multi-select adjacent items
+        </li>
+      </ul>
+    </div>
     <div class="flex items-center gap-x-4 mt-5">
       <button
         type="button"
@@ -124,3 +147,9 @@ onMounted(() => {
   })
 })
 </script>
+
+<style lang="postcss">
+kbd {
+  @apply bg-gray-100 rounded border-gray-300 shadow border p-1 text-xs;
+}
+</style>
